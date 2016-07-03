84a9135d1f558e1707ccffa0966063f371c64b4e
teseo <sol@javiermellado.com>
2016-07-03 19:10:07 +0100

    refactor gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,17 +2,19 @@
 var gulp    = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
-// run phpunit tests
-require('./gulp-tasks/matrix-difference')(gulp, plugins);
-require('./gulp-tasks/watch-matrix-difference')(gulp);
-
-//clear screen everytime tasks is performed
+//clear screen task
 gulp.task('clear', function () {
     gulp.src('').pipe(plugins.run('clear'));
 });
 
-// default task: clear screen, run test, watch files for modifications
-gulp.task('default', [
+//Matrix difference challenge tasks
+require('./gulp-tasks/matrix-difference')(gulp, plugins);
+require('./gulp-tasks/watch-matrix-difference')(gulp);
+
+gulp.task('matrix-difference', [
     'clear',
     'suite-matrix-difference',
     'watch-suite-matrix-difference']);
+
+// default task: clear screen, run test suites
+gulp.task('default', ['matrix-difference']);
