e227b8a0c7621d90b6b51eb538efbad0a5cd5055
teseo <sol@javiermellado.com>
2016-07-03 21:22:39 +0100

    Challenge PlusMinus finished

--- src/PlusMinus/PlusMinus.php
+++ src/PlusMinus/PlusMinus.php
@@ -3,5 +3,117 @@ namespace PlusMinus;
 
 class PlusMinus
 {
-    
+    private $positiveNumbers = 0;
+    private $zeroNumbers     = 0;
+    private $negativeNumbers = 0;
+    private $totalElements   = 0;
+
+    public function parse($inputArray)
+    {
+        $this->totalElements = count($inputArray);
+
+        foreach ($inputArray as $item) {
+            if ($this->isPositive($item))
+            {
+                $this->positiveNumbers++;
+            }
+            elseif ($this->isNegative($item))
+            {
+                $this->negativeNumbers++;
+            }
+            else
+            {
+                $this->zeroNumbers++;
+            }
+        }
+
+        return  $this->printOutput();
+    }
+
+    /**
+     * @return string
+     */
+    private function printOutput()
+    {
+        $result = '';
+
+        if($this->positiveNumbers > 0)
+        {
+            $result.= $this->formatPositiveNumbers() . PHP_EOL;
+        }
+        else
+        {
+            $result.= $this->pritEmptyDefaultValue() . PHP_EOL;
+        }
+
+        if($this->negativeNumbers > 0)
+        {
+            $result.= $this->formatNegativeNumbers() . PHP_EOL;
+        }
+        else
+        {
+            $result.= $this->pritEmptyDefaultValue() . PHP_EOL;
+        }
+
+        if($this->zeroNumbers > 0)
+        {
+            $result.= $this->formatZeroNumbers();
+        }
+        else
+        {
+            $result.= $this->pritEmptyDefaultValue();
+        }
+        
+        return  $result;
+    }
+
+    /**
+     * @param $item
+     * @return bool
+     */
+    private function isPositive($item)
+    {
+        return $item > 0;
+    }
+
+    /**
+     * @param $item
+     * @return bool
+     */
+    private function isNegative($item)
+    {
+        return $item < 0;
+    }
+
+    /**
+     * @return string
+     */
+    private function formatPositiveNumbers()
+    {
+        return number_format($this->positiveNumbers / $this->totalElements, 6);
+    }
+
+    /**
+     * @return string
+     */
+    private function pritEmptyDefaultValue()
+    {
+        return number_format(0, 6);
+    }
+
+    /**
+     * @return string
+     */
+    private function formatNegativeNumbers()
+    {
+        return number_format($this->negativeNumbers / $this->totalElements, 6);
+    }
+
+    /**
+     * @return string
+     */
+    private function formatZeroNumbers()
+    {
+        return number_format($this->zeroNumbers / $this->totalElements, 6);
+    }
 }
--- tests/PlusMinusTest/PlusMinusTest.php
+++ tests/PlusMinusTest/PlusMinusTest.php
@@ -4,7 +4,7 @@ use PlusMinus\PlusMinus;
 
 class PlusMinusTest extends PHPUnit_Framework_TestCase
 {
-    /** @var  PlusMinus $matrix */
+    /** @var  PlusMinus $plusmMinus */
     private $plusmMinus;
 
     /**
@@ -21,5 +21,34 @@ class PlusMinusTest extends PHPUnit_Framework_TestCase
     public function testPlusMinusCanBeResolved()
     {
         $this->assertTrue($this->plusmMinus instanceof \PlusMinus\PlusMinus);
+    }
+
+    public function testReturnForArrayContainingOnlyOneZero()
+    {
+        $input = [0];
+        $this->assertEquals('0.000000' . PHP_EOL .'0.000000' . PHP_EOL .'1.000000', $this->plusmMinus->parse($input));
+    }
+    public function testReturnForArrayContainingOnlyOneNegativeNumber()
+    {
+        $input = [-1];
+        $this->assertEquals('0.000000' . PHP_EOL .'1.000000' . PHP_EOL .'0.000000', $this->plusmMinus->parse($input));
+    }
+
+    public function testReturnForArrayContainingNoNegativeNumbers()
+    {
+        $input = [1,0];
+        $this->assertEquals('0.500000' . PHP_EOL . '0.000000' . PHP_EOL . '0.500000', $this->plusmMinus->parse($input));
+    }
+
+    public function testReturnThreelinesInArrayOfSixElementsOfAllKinds()
+    {
+        $input = [-4, 3, -9, 0, 4, 1];
+        $this->assertEquals('0.500000' . PHP_EOL . 0.333333 . PHP_EOL. 0.166667 , $this->plusmMinus->parse($input));
+    }
+    public function testReturnForSevenElementsWithOutZeroValues()
+    {
+        $input = [1, -2, -7, 9, 1, -8, -5];
+
+        $this->assertEquals(0.428571 . PHP_EOL . 0.571429 . PHP_EOL. '0.000000' , $this->plusmMinus->parse($input));
     }
 }
